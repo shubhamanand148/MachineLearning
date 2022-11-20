f18a5f93feb179f404b5638cade49163a88531f4
Shubham Anand <shubhamanand148@gmail.com>
2022-11-20 23:00:05 +0530

    MLGitHub

--- SVM/Digit_ReconitionSVM.py
+++ SVM/Digit_ReconitionSVM.py
@@ -20,8 +20,15 @@ plt.show()
 
 #The classifier works in 1D array.
 #Transforming/Flattening the 8x8 image to 1D array with 64 elements.
-data = digit_data.images.reshape((len(digit_data.images), -1))
+flat_data = digit_data.images.reshape((len(digit_data.images), -1))
 
 classifier = svm.SVC(gamma=0.001)
 
 train_test_split = int(len(digit_data.images) * 0.8)
+classifier.fit(flat_data[:train_test_split], digit_data.target[:train_test_split])
+
+target_value = digit_data.target[train_test_split:]
+predicted_value = classifier.predict(flat_data[train_test_split:])
+
+print("Confusion Matrix: \n %s" %metrics.confusion_matrix(target_value, predicted_value))
+print("Accuracy: ", accuracy_score(target_value, predicted_value))
